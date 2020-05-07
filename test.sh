b1a17018c2b637d5b39c1eb9675810a2600cd93b
stackerzzq <foo_stacker@yeah.net>
2020-05-08 00:44:32 +0800

    better test shell

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-case $1 in
+case $2 in
     "pub")
-    ./test-nsqd-pub 192.168.0.100 sample_topic
+    ./test-nsqd-pub $1 sample_topic
     ;;
     "sub")
-    ./test-nsqd-sub 192.168.0.100 sample_topic ch
+    ./test-nsqd-sub $1 sample_topic ch
     ;;
     "lookupd-sub")
-    ./test-lookupd-sub 192.168.0.100 sample_topic sample_channel
+    ./test-lookupd-sub $1 sample_topic ch
     ;;
     *)
-    ./test-nsqd-pub 192.168.0.100 sample_topic
-    ./test-nsqd-sub 192.168.0.100 sample_topic sample_channel
+    ./test-nsqd-pub $1 sample_topic
+    ./test-nsqd-sub $1 sample_topic sample_channel
     ;;
 esac
